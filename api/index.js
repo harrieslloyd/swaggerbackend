e66a6bd4cf178d2d1d65827608fa3ae51f14e311
Harries Lloyd <harrieslloyd9@gmail.com>
2023-11-03 12:30:04 -0600

    testing secrets

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ app.get('/api', (req, res) => {
 
 app.get('/api/item/:slug', (req, res) => {
   const { slug } = req.params;
-  res.end(`Item: ${slug}`);
+  res.end(process.env.CLIENTSECRET);
 });
 
 module.exports = app;
